cdf6e1e0ead0f480717e42ef3734345833ffa4d7
Jonathan-Kang <ymkang1630@gmail.com>
2022-11-25 15:33:53 -0300

    feat(add view manueal):

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("Glauben/", views.Glauben),
+    path("login/", views.login_view),
+    path("GlaubenLogin/", views.GlaubenLogin_view),
+    path("sidebarGlauben/", views.login_Sidebar),
+    path("estadistica/", views.graficoBar)
+]
